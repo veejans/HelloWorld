33bc83321f4b1f532ceb34b7209965cb2d9e8899
veejans <58972339+veejans@users.noreply.github.com>
2019-12-17 15:05:08 +0530

    Update and rename Hello.java to HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,6 @@
 /* This is a simple Java program. 
 FileName : "HelloWorld.java". */
-class Hello
+class HelloWorld
 { 
 	// Your program begins with a call to main(). 
 	// Prints "Hello, World" to the terminal window. 
